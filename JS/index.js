a8465ad44da533855d06d3d0d46a160a759e9035
Cintia-Godoy <137214626+Cintia-Godoy@users.noreply.github.com>
2024-11-10 17:29:42 +0000

    Added JS for current date and time

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  let londonElement = document.querySelector("#london");
 
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonClock = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonClock.format("MMMM	Do YYYY");
+  londonTimeElement.innerHTML = londonClock.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let rioElement = document.querySelector("#rio");
+
+  let rioDateElement = rioElement.querySelector(".date");
+  let rioTimeElement = rioElement.querySelector(".time");
+  let rioClock = moment().tz("Brazil/East");
+
+  rioDateElement.innerHTML = rioClock.format("MMMM	Do YYYY");
+  rioTimeElement.innerHTML = rioClock.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
